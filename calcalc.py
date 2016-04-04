7b218c3702a935adac6907d893e895711970fce6
Sempoo <sempoo@gmail.com>
2016-04-04 21:58:38 +0200

    still problems with negative numbers

--- calcalc.py
+++ calcalc.py
@@ -1,100 +1,116 @@
-# I want it to be a special calculator, which is entirely based on addition;
-# so for example multiplication is a repeated form of addition, and so on...
+a = -2
+b = 3
 
-a = 0.1
-b = 1
-
-print('a =', a)
-print('b =', b)
-print('reference_result =', a * b)
+# TODO: problems with negative numbers - try to use abs()
+# TODO: and then add back the minus sign
 
 
 def multiplier_finder(x):
+    print('\n-- multiplier_finder(x) --')
+
     string_version = str(x)
-    dot_index = string_version.find('.') + 1
-    num_length = len(string_version)
-    print('###')
     print('string_version =', string_version)
-    print('string_version[0] =', string_version[0])
-    print('dot_index =', dot_index)
-    print('num_length =', num_length)
-    print('###')
-    if string_version.find('.') >= 0:
-        multiplier = '1' + multiplication((num_length-dot_index), '0')
+
+    if string_version.find('.') > 0:
+        print('string has dot')
+
+        if string_version.find('-') == 0:
+            print('string has minus')
+            digits = len(string_version) - 2
+            print('digits =', digits)
+            digits_after_dot = digits - string_version.find('.') + 1
+            print('digits_after_dot =', digits_after_dot)
+            multiplier = int('1' + multiplication(digits_after_dot, '0'))
+            print('multiplier =', multiplier)
+            return multiplier, digits_after_dot
+        else:
+            digits = len(string_version) - 1
+            print('digits =', digits)
+            digits_after_dot = digits - string_version.find('.')
+            print('digits_after_dot =', digits_after_dot)
+            multiplier = int('1' + multiplication(digits_after_dot, '0'))
+            print('multiplier =', multiplier)
+            return multiplier, digits_after_dot
+
+    else:
+        print('string is integer')
+        multiplier = 1
         print('multiplier =', multiplier)
-        print('###')
-        return int(multiplier), len(multiplier)-1
-    if string_version[0] == '0':
-        multiplier = '1' + multiplication((num_length-dot_index), '0')
-        return int(multiplier), len(multiplier)
-    else:
-        return 1, 0
+        return multiplier, 0
 
 
-
-
-def multiplication(a, b):
-    c = b
-    if a == 0:
+def multiplication(x, y):
+    print('\n-- multiplication(x, y) --')
+    z = y
+    if x == 0:
         return 0
-    if b == 0:
+    if y == 0:
         return 0
-    print('str(a)[0] =', (str(a)[0]))
-    if str(a)[0] == '0':
-        return b
-    if int(a) != a:
-        integer_part = int(a)
-        float_remainder = a - integer_part
+    if int(x) != x:
+        print('x is float')
+        integer_part = int(x)
+        print('integer_part =', integer_part)
+        float_remainder = round(x - integer_part, multiplier_finder(x)[1])
+        print('float_remainder =', float_remainder)
         for i in range(integer_part-1):
-            b += c
-
-        return b + multiplication(c, float_remainder)
+            y += z
+        if integer_part == 0:
+            return round(multiplication(z, float_remainder))
+        else:
+            return y + round(multiplication(z, float_remainder))
     else:
-        integer_part = a
+        print('x is integer')
+        integer_part = x
+        print('integer_part =', integer_part)
         for i in range(integer_part-1):
-            b += c
-        return b
+            y += z
+        return y
 
 
 def float_result(integer_result, decimal_places):
+    print('\n-- float_result(ab_integer_multiplication, length_final) --')
     string_version = str(integer_result)
-    radix_point_index = -decimal_places
+    print('string version =', string_version)
+    print('decimal_places =', decimal_places)
+    radix_point_index = - decimal_places
+    print('radix_point_index =', radix_point_index)
     if radix_point_index == 0:
         return integer_result
     decimal_part = string_version[:radix_point_index]
-    fractional_part = string_version[radix_point_index:]
-    number = float(decimal_part + '.' + fractional_part)
-    print('###')
-    print('string_version =', string_version)
-    print('radix_point_index =', radix_point_index)
     print('decimal_part =', decimal_part)
+    fractional_part = string_version.zfill(decimal_places)[radix_point_index:]
     print('fractional_part =', fractional_part)
-    print('###')
+    number = float(decimal_part + '.' + fractional_part)
     return number
 
 
+multiplier_a = multiplier_finder(a)[0]
+multiplier_b = multiplier_finder(b)[0]
+print('multiplier_a =', multiplier_a)
+print('multiplier_b =', multiplier_b)
 
-
-multiplier_a = multiplier_finder(a)
-multiplier_b = multiplier_finder(b)
-print('multiplier_a =', multiplier_a[0])
-print('multiplier_b =', multiplier_b[0])
-
-integer_a = int(multiplication(a, multiplier_a[0]))
-integer_b = int(multiplication(b, multiplier_b[0]))
+integer_a = multiplication(a, multiplier_a)
+integer_b = multiplication(b, multiplier_b)
 print('integer_a =', integer_a)
 print('integer_b =', integer_b)
+print('type of integer_a =', type(integer_a))
+print('type of integer_b =', type(integer_b))
 
-length_a = multiplier_a[1]
-length_b = multiplier_b[1]
+length_a = multiplier_finder(a)[1]
+length_b = multiplier_finder(b)[1]
 print('length_a =', length_a)
 print('length_b =', length_b)
-
-integers_multiplication = multiplication(integer_a, integer_b)
-print('integers_multiplication =', integers_multiplication)
 
 length_final = length_a + length_b
 print('length_final =', length_final)
 
-final_result = float_result(integers_multiplication, length_final)
-print('\n\nfinal_result =', final_result)
+ab_integer_multiplication = multiplication(integer_a, integer_b)
+print('integer_a =', integer_a)
+print('integer_b =', integer_b)
+print('ab_integer_multiplication =', ab_integer_multiplication)
+
+final_result = float_result(ab_integer_multiplication, length_final)
+print('\na =', a)
+print('b =', b)
+print('reference result =', a * b)
+print('\n__FINAL RESULT__ =', final_result)
